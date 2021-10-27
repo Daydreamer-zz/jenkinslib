6aa9f790643d66248e299a41aa8c79bf89286275
shz <shz1997@hotmail.com>
2021-10-27 14:31:21 +0800

    channge some

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -19,7 +19,7 @@ def SonarScan(sonarServer,projectName,projectDesc,projectPath,branchName){
         -Dsonar.ws.timeout=30 \
         -Dsonar.projectDescription="${projectDesc}" \
         -Dsonar.links.homepage=http://www.baidu.com \
-        -Dsonar.sources=${projectPath} \
+        -Dsonar.sources="${projectPath}" \
         -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes \
         -Dsonar.java.test.binaries=target/test-classes \
         -Dsonar.java.surefire.report=target/surefire-reports  \
