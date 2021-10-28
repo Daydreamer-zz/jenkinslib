c6ff8d09b134c31df39971eea6e0612e695a6dde
shz <shz1997@hotmail.com>
2021-10-28 10:13:38 +0800

    sonarqube status

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -31,8 +31,8 @@ def SonarScan(sonarServer,projectName,projectDesc,projectPath,branchName){
         """
     }
     
-    //def qg = waitForQualityGate()
-    //if (qg.status != 'OK') {
-        //error "Pipeline aborted due to quality gate failure: ${qg.status}"
-    //}
+    def qg = waitForQualityGate()
+    if (qg.status != 'OK') {
+        error "Pipeline aborted due to quality gate failure: ${qg.status}"
+    }
 }
